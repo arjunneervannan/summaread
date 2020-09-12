51b52d8543615752a56eb990484ff78212da8b0b
Brandon Wang <brandonbigbrother@gmail.com>
2020-09-12 17:24:48 -0500

    rename a temp file

--- src/slide_change_gcp.py
+++ src/slide_change_gcp.py
@@ -1,95 +1,51 @@
-from google.cloud import videointelligence
-import os
+from google.cloud import language
 import pickle
-import youtube_dl
-from google.cloud import storage
+import os
 
+"""
+Works well on first part of this lecture (test_text.txt)
+https://www.youtube.com/watch?v=XbIfFY_fJ_s
 
-def my_hook(d):
-    if d['status'] == 'finished':
-        print('Done downloading, now uploading ...')
-
-
-def download_vid(url):
-    # delete old video/subtitles
-    if os.path.exists("../videos/video123.en-US.vtt"):
-        os.remove("../videos/video123.en-US.vtt")
-    if os.path.exists("../videos/video123.mp4"):
-        os.remove("../videos/video123.mp4")
-
-    # upload new video
-    ydl_opts = {
-        'format': '18',
-        'outtmpl': '../videos/video123.mp4',
-        'noplaylist': True,
-        'writesubtitles': True,
-        'allsubtitles': True,
-        'progress_hooks': [my_hook],
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
-
-def upload_blob(source_file_name, destination_blob_name):
-    """Uploads a file to the bucket."""
-    bucket_name = "hackathon_sahitya"
-    # source_file_name = "local/path/to/file"
-    # destination_blob_name = "storage-object-name"
-
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-
-    blob.upload_from_filename(source_file_name)
-
-    print(
-        "File {} uploaded to {}.".format(
-            source_file_name, destination_blob_name
-        )
-    )
-
-
-# set access to my google API key
+"""
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../gckey.json"
 
 
-def process_vid(path_url):
-    """ Detects camera shot changes. """
-    video_client = videointelligence.VideoIntelligenceServiceClient()
-    features = [videointelligence.enums.Feature.SHOT_CHANGE_DETECTION]
-    operation = video_client.annotate_video(input_uri=path_url, features=features)
-    print("\nProcessing video for shot change annotations:")
+def process_file(path):
+    client = language.LanguageServiceClient()
 
-    result = operation.result(timeout=90)
-    print("\nFinished processing.")
+    file = open(path, "r")
+    text = file.read()
 
-    shot_list = []
+    document = language.types.Document(
+        content=text,
+        type=language.enums.Document.Type.PLAIN_TEXT,
+    )
+    response = client.analyze_entities(
+        document=document,
+        encoding_type='UTF32',
+    )
+    wikipedia_entities = {}
 
-    # first result is retrieved because a single video was processed
-    for i, shot in enumerate(result.annotation_results[0].shot_annotations):
-        start_time = shot.start_time_offset.seconds + shot.start_time_offset.nanos / 1e9
-        end_time = shot.end_time_offset.seconds + shot.end_time_offset.nanos / 1e9
-        print("\tShot {}: {} to {}".format(i, start_time, end_time))
-        shot_list.append((start_time, end_time))
+    for entity in response.entities:
+        if 'wikipedia_url' in entity.metadata:
+            # print('=' * 20)
+            # print('         name: {0}'.format(entity.name))
+            # print('         type: {0}'.format(entity.type))
+            # print('     metadata: {0}'.format(entity.metadata))
+            # print('     salience: {0}'.format(entity.salience))
+            wikipedia_entities[entity.name] = entity.metadata['wikipedia_url']
 
-    pickle.dump(shot_list, open("../videos/slide_cuts.p", "wb"))
+    pickle.dump(wikipedia_entities, open("../videos/note_entities.pkl", "wb"))
     print("All done!")
+    return wikipedia_entities
 
 
 def load_pkl():
-    shot_list = pickle.load(open("../videos/slide_cuts.p", "rb"))
-    print(shot_list)
-
-
-def video_to_transcript_cuts(video_url):
-    download_vid(video_url)
-    upload_blob("../videos/video123.mp4", "video123.mp4")
-    path = "gs://hackathon_sahitya/video123.mp4"
-    process_vid(path)
-    # load_pkl()
+    entities = pickle.load(open("../videos/note_entities.pkl", "rb"))
+    print(entities)
+    return entities
 
 
 if __name__ == "__main__":
-    video_url = 'https://www.youtube.com/watch?v=XbIfFY_fJ_s'
-    video_to_transcript_cuts(video_url)
+    process_file("../videos/summary.txt")
+    load_pkl()
