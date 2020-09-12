db5be27a5bf176379d54c7ede83d788b99c4e405
matt-dong <mattdong03@gmail.com>
2020-09-12 17:00:42 -0400

    logo display'

--- src/group_lines.py
+++ src/group_lines.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 19:42:52 2020
+
+@author: mattd
+"""
+import webvtt
+
+def reformat_transcription(file_name):
+
+    text = webvtt.read( "src/" + file_name)
+    caption_list = []
+    for line in text:
+        caption_list.append((line.start, line.text))
+               
+    return(caption_list)
+
+def seconds_to_time(file_name):
+
+
 def group_lines(file_name, increment):
     text = webvtt.read( "src/" + file_name)
 
