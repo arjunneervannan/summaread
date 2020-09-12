03d142faba7ffc1e704e28566926382326243516
Brandon Wang <brandonbigbrother@gmail.com>
2020-09-11 20:51:20 -0500

    added first draft of main page

--- app.py
+++ app.py
@@ -3,17 +3,17 @@
 
 from flask import Flask, render_template
 
-DEVELOPMENT_ENV  = True
+DEVELOPMENT_ENV = True
 
 app = Flask(__name__)
 
 app_data = {
-    "name":         "Peter's Starter Template for a Flask Web App",
-    "description":  "A basic Flask app using bootstrap for layout",
-    "author":       "Peter Simeth",
-    "html_title":   "Peter's Starter Template for a Flask Web App",
-    "project_name": "Starter Template",
-    "keywords":     "flask, webapp, template, basic"
+    "name": "Hackathon Project",
+    "description": "Hackathon Project made by the DM team",
+    "author": "Discreet Math",
+    "html_title": "Hackathon Project made by the DM team",
+    "project_name": "Note Summarizer",
+    "keywords": "flask, webapp, template, basic"
 }
 
 
@@ -35,6 +35,11 @@ def service():
 @app.route('/contact')
 def contact():
     return render_template('contact.html', app_data=app_data)
+
+
+@app.route('/results')
+def results():
+    return render_template('results.html', app_data=app_data)
 
 
 if __name__ == '__main__':
